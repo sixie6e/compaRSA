f146ccaf705ff75e2aadbbd65ac82a1a1348b1cb
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2025-12-31 07:34:28 -0500

    no need for f strings

--- shwrite.py
+++ shwrite.py
@@ -1,5 +1,5 @@
-with open(f'selections_history.txt', 'r') as shread:
-    with open(f'sh.txt', 'w+') as shwrite:
+with open('selections_history.txt', 'r') as shread:
+    with open('sh.txt', 'w+') as shwrite:
         for line in shread:
             if "Length: 0" not in line:
                 shwrite.write(line)
