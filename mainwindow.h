9014059c8d5a7303cb88066bbcf3b4f15f848f2f
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-01-13 13:51:38 -0500

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <gmpxx.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <atomic>
+
+typedef mpz_class BigInt;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+signals:
+    void progressUpdated(int value);
+    void statusLog(QString msg);
+    void calculationFinished();
+
+private slots:
+    void on_runButton_clicked();
+    void on_stopButton_clicked();
+    void updateProgressBar(int value);
+    void handleLog(QString msg);
+    void onRunComplete();
+
+private:
+    Ui::MainWindow *ui;
+
+    std::map<std::string, std::vector<BigInt>> sets;
+    int next_set = 23;
+    std::atomic<bool> stop_requested;
+
+    void initialize_base_sets();
+    bool is_prime(const BigInt& n);
+    void process_pair(std::string i, std::string j, int limit);
+    void runAutoMode();
+    void save_state();
+};
+
+#endif // MAINWINDOW_H
