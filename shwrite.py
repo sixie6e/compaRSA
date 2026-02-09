a71c646c3baba5230230a1a91e0fd585a0f20f19
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-02-09 15:38:43 -0500

    rem shwrite.py

--- shwrite.py
+++
@@ -1,5 +0,0 @@
-with open('selections_history.txt', 'r') as shread:
-    with open('sh.txt', 'w+') as shwrite:
-        for line in shread:
-            if "Length: 0" or "Length: 1" not in line:
-                shwrite.write(line)
