5473eb289f267a152bdde83e3713e7a7e8ee2219
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-02-14 08:58:16 -0500

    Update parquet_plt.py

--- parquet_plt.py
+++ parquet_plt.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import pyarrow.parquet as pq
+from tkinter import simpledialog as sd
 
 def cpp_plt(filename):
     table = pq.read_table(filename, thrift_string_size_limit=100*1024*1024, thrift_container_size_limit=100*1024*1024)
     df = table.to_pandas()
-    
-    print(f"File has {len(df.columns)} sets available.")
 
-    try:
-        setA = int(input("Set A: "))
-        setB = int(input("Set B: "))
-    except ValueError:
-        print("Invalid.")
-        return
+    setA = sd.askinteger("setA", f"File has {len(df.columns)} sets available. \n\nSet A: ")
+    setB = sd.askinteger("setB", f"File has {len(df.columns)} sets available. \n\nSet B: ")
 
     selected_columns = df.columns[setA:setB]
 
@@ -21,7 +16,7 @@ def cpp_plt(filename):
 
     for column in selected_columns:
         data = pd.to_numeric(df[column], errors='coerce').dropna()
-        
+
         if not data.empty:
             plt.plot(data.index, data.values, label=column,
                      marker='o', linestyle='solid', markersize=2)
@@ -33,7 +28,8 @@ def cpp_plt(filename):
     plt.legend(loc='best', bbox_to_anchor=(1.15, 1))
     plt.grid(True, which="both", ls="-", alpha=0.5)
     plt.tight_layout()
-    plt.show() 
-    
+    plt.show()
+
+
 if __name__ == "__main__":
     cpp_plt("export.parquet")
